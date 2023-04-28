feb5fbf6c7a9e0747c7e5bcf46819945ed730e0b
Hans Emmanuel E. Duran <hansemmanuel.duran@cit.edu>
2023-04-28 15:41:41 +0800

    Initial commit

---
+++ Customers/Contexts/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Customers.Models;
+
+namespace Customers.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
