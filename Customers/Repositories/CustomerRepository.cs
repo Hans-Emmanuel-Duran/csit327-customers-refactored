05229725dbf6d8ff0d31e6f3d1b1c1626c546bde
Hans Emmanuel E. Duran <hansemmanuel.duran@cit.edu>
2023-05-14 15:24:56 +0800

    Add customer repository

---
+++ Customers/Repositories/CustomerRepository.cs
@@ -0,0 +1,43 @@
+﻿using Customers.Contexts;
+using Customers.Models;
+
+namespace Customers.Repositories
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        private CustomerContext _context;
+
+        public CustomerRepository(CustomerContext context)
+        {
+            _context = context;
+        }
+
+        public Customer Get(int id)
+        {
+            return _context.Customers.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public void Create(Customer customer)
+        {
+            _context.Customers.Add(customer);  
+            _context.SaveChanges();
+        }
+        public void Update(Customer customer, Customer updatedCustomer)
+        {
+            customer.Name = updatedCustomer.Name;
+            customer.Age = updatedCustomer.Age;
+            _context.SaveChanges();
+        }
+
+        public void Delete(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+        }
+
+    }
+}
---
+++ Customers/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Customers.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        void Create(TEntity customer);
+        void Update(TEntity customer, TEntity updatedCustomer);
+        void Delete(TEntity customer);
+    }
+}
